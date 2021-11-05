2600fca1193faec51b5957f205a49c7d0ce6cef1
Gustavo Roger Creutzberg <gustavo.creutzberg@gmail.com>
2021-11-04 22:26:12 -0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Inicio from './Home/Inicio';
+import Header from './Jogo/Header';
 import Categorias from './Jogo/Categorias';
 import Perguntas from './Jogo/Perguntas';
 import Ranking from './Jogo/Ranking';
@@ -8,6 +9,9 @@ import Resultado from './Jogo/Resultado';
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <Header/>
+      </header>
       <Inicio/>
       <Categorias/>
       <Perguntas/>
---
+++ src/Jogo/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = props => {
+    return (
+        <h1>MEU MENU</h1>
+    );
+}
+
+export default Header;
