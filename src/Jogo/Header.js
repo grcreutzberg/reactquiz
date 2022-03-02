37a56e313cc732cd70e6ed35c1bed22593909e99
Gustavo Roger Creutzberg <gustavo.creutzberg@gmail.com>
2022-03-01 21:02:05 -0300

    Navegação menu

--- src/Jogo/Header.js
+++ src/Jogo/Header.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Image, Menu } from 'semantic-ui-react';
 
 const Header = props => {
     return (
-        <h1>MEU MENU</h1>
+        <Menu>
+            <Menu.Item><strong>Quiz</strong></Menu.Item>
+            <Menu.Item as={Link} to={'/'}>Home</Menu.Item>
+            <Menu.Item as={Link} to={'/categorias'}>Categorias</Menu.Item>
+            <Menu.Item as={Link} to={'/perguntas'}>Perguntas</Menu.Item>
+            <Menu.Item as={Link} to={'/resultado'}>Resultado</Menu.Item>
+            <Menu.Item as={Link} to={'/ranking'}>Ranking</Menu.Item>
+            <Menu.Menu position='right'>
+                <Menu.Item>
+                    <Image avatar src=''/>
+                    <strong>Gustavo</strong>
+                </Menu.Item>
+            </Menu.Menu>
+        </Menu>
     );
 }
 
